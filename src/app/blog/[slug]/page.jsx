5d6d0712e303e9ab2ed3604078751ba22a78f8d4
karimou cisse <karimou.cisse1@gmail.com>
2024-04-16 20:30:49 +0200

    add new file in blog folder

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import Image from "next/image"
+import styles from "./singlePost.module.css"
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className= {styles.container}>
+      <div className={styles.left}>
+        <Image src= "/photo.jpg" fill/>
+      </div>
+      <div className= {styles.right}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+          libero eligendi dignissimos autem hic? Quos animi, deleniti laborum a
+          , iure, voluptatum numquam nihil possimus asperiores facilis maxime
+           harum voluptate veritatis.
+        </p>
+      </div>
+    </div>
   )
 }
 
