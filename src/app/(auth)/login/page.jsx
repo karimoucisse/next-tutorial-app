4580fc2de71b95dc4321534d77ee0f0ee1e171c9
karimou cisse <karimou.cisse1@gmail.com>
2024-04-19 19:49:51 +0200

    add change in login file

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from "./login.module.css"
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className= {styles.container}>
+      <form action="">
+        <input type="string" placeholder="Name"/>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password"/>
+        <button>Valider</button>
+      </form>
+    </div>
   )
 }
 
